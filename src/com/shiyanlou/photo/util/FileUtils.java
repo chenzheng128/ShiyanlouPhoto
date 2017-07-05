729e408ce17b51ce533fbe8099e389d8871266f7
ZhengChen_mini <chenzheng128@yeah.net>
2017-07-05 16:51:02 +0800

    禁用了七牛api src/com/shiyanlou/photo/util/FileUtils.java
    禁用了七牛api, 解除了与 qinniu.jar 的依赖关系

--- src/com/shiyanlou/photo/util/FileUtils.java
+++ src/com/shiyanlou/photo/util/FileUtils.java
@@ -4,15 +4,11 @@ import java.io.InputStream;
 
 import org.json.JSONException;
 
-import com.qiniu.api.auth.AuthException;
-import com.qiniu.api.auth.digest.Mac;
-import com.qiniu.api.io.IoApi;
-import com.qiniu.api.rs.PutPolicy;
-import com.qiniu.api.rs.RSClient;
-
 /**
  * 图片工具类（使用七牛云存储服务）
  * @author www.shiyanlou.com
+ * 
+ * @deprecated 七牛云的API jar 已过期, 不再使用其API, 这个类也不再使用
  *
  */
 public class FileUtils {
@@ -26,28 +22,30 @@ public class FileUtils {
      * 上传图片到七牛云存储
      * @param reader
      * @param fileName
+     * @deprecated
      */
     public static void upload(InputStream reader, String fileName) {
         String uptoken;
-        try {
-            Mac mac = new Mac(ACCESS_KEY, SECRET_KEY);
-            PutPolicy putPolicy = new PutPolicy(BUCKET_NAME);
-            uptoken = putPolicy.token(mac);
-            IoApi.Put(uptoken, fileName, reader, null);
-        } catch (AuthException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Mac mac = new Mac(ACCESS_KEY, SECRET_KEY);
+//            PutPolicy putPolicy = new PutPolicy(BUCKET_NAME);
+//            uptoken = putPolicy.token(mac);
+//            IoApi.Put(uptoken, fileName, reader, null);
+//        } catch (AuthException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 
     /**
      * 删除七牛云存储上的图片
      * @param key
+     * @deprecated
      */
     public static void delete( String key) {
-        Mac mac = new Mac(ACCESS_KEY, SECRET_KEY);
-        RSClient client = new RSClient(mac);
-        client.delete(BUCKET_NAME, key);
+//        Mac mac = new Mac(ACCESS_KEY, SECRET_KEY);
+//        RSClient client = new RSClient(mac);
+//        client.delete(BUCKET_NAME, key);
     }
 }
